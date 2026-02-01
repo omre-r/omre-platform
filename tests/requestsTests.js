603316ca42c13b7521274ca4d659238e02a93eb1
saleh-programs <muradsaleh2022@gmail.com>
2026-02-01 16:40:32 -0500

    Created random test flow

---
+++ tests/requestsTests.js
@@ -0,0 +1,77 @@
+const   {
+    validateLoginReq, changePasswordReq, getUserReq, getUsersReq, createUserReq,
+    getProductReq, updateProductReq, deleteProductReq, getActiveProductsReq, createProductReq, getProductsReq,
+    getProductReviewsReq, getUserReviewsReq, updateReviewReq, createReviewReq, getReviewsReq, 
+    cancelOrderReq, completeOrderReq, getOrderReq, createOrderReq
+} = require("../frontend/src/requests.js")
+
+
+// Testing a user flow
+
+async function testUserFlow(){
+    //email, password, firstname, lastname, role
+    const user1 = {
+        email: "salehm0529@gmail.com",
+        password: "crzy8123",
+        firstname: "murad",
+        lastname: "saleh",
+        role: "user"
+    }
+    const user2 = {
+        email: "ahadkidwai45@gmail.com",
+        password: "123",
+        firstname: "ahad",
+        lastname: "kidwai",
+        role: "user"
+    }
+    const user3 = {
+        email: "aymannazir28@gmail.com",
+        password: "321",
+        firstname: "ayman",
+        lastname: "nazir",
+        role: "user"
+    }
+    const user4 = {
+        email: "zanechriste88@gmail.com",
+        password: "helloworld",
+        firstname: "zane",
+        lastname: "christe",
+        role: "user"
+    }
+
+    let allUsers
+    //get all users
+    console.log("Getting all users: ", await getUsersReq())
+
+    //create 4 users
+    const createdUser1 =  await createUserReq(user1);
+    const createdUser2 =  await createUserReq(user2);
+    const createdUser3 =  await createUserReq(user3);
+    const createdUser4 =  await createUserReq(user4)
+    console.log("Created users", createdUser1, createdUser2, createdUser3, createdUser4)
+
+    //get all users
+    console.log("Getting all users: ", await getUsersReq())
+
+    //get user 2
+    console.log("Getting user 2", await getUserReq(createdUser2.id))
+
+    // //delete user 2
+    // await (createdUser2.id)
+    // console.log("Deleting user 2")
+
+    // All users login with wrong credentials
+    let retrievedUser1 = await validateLoginReq(createdUser1.id, createdUser1.email, "no")
+    let retrievedUser2 = await validateLoginReq(createdUser2.id, createdUser2.email, "dewjrdncw")
+    let retrievedUser3 = await validateLoginReq(createdUser3.id, createdUser3.email, "asx")
+    let retrievedUser4 = await validateLoginReq(createdUser4.id, createdUser4.email, "xsaxed")
+    console.log("Retrieved users (wrong login): ", retrievedUser1, retrievedUser2, retrievedUser3, retrievedUser4)
+
+    //All users login with correct credentials
+    retrievedUser1 = await validateLoginReq(createdUser1.id, createdUser1.email, user1.password)
+    retrievedUser2 = await validateLoginReq(createdUser2.id, createdUser2.email, user2.password)
+    retrievedUser3 = await validateLoginReq(createdUser3.id, createdUser3.email, user3.password)
+    retrievedUser4 = await validateLoginReq(createdUser4.id, createdUser4.email, user4.password)
+    console.log("Retrieved users (correct login): ", retrievedUser1, retrievedUser2, retrievedUser3, retrievedUser4)
+}
+testUserFlow()
