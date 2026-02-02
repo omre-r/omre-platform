76534ab0e96c3c89c79d82932400aa6917896cb1
xp-squared <108478666+xp-squared@users.noreply.github.com>
2026-02-02 14:46:04 -0500

    Incomplete products panel Will be updated shortly have to pull some files from dev

--- frontend/src/AdminComponents/ProductsPanel.jsx
+++ frontend/src/AdminComponents/ProductsPanel.jsx
@@ -1,13 +1,272 @@
-import React, { useState } from 'react';
-import { Button, Card, Heading, Text, View } from '@aws-amplify/ui-react';
+import { useEffect, useState } from "react";
+import { Card, Flex, Text, Button, View } from "@aws-amplify/ui-react";
+
+import {getProductReq, updateProductReq, deleteProductReq, getActiveProductsReq, getProductsReq, createProductReq} from'../requests.js';
+
+
+// Custom Styling for fonts and amplify ui --------------------------------------
+const luxuryHeadingStyle = {
+  fontFamily: "'Cormorant Garamond', serif",
+  fontWeight: 600,
+  fontSize: "1.5rem",
+  letterSpacing: "0.5px",
+};
+const luxuryBodyStyle = {
+  fontFamily: "'Cormorant Garamond', serif",
+  fontWeight: 400,
+  fontSize: "1.3rem",   
+  letterSpacing: "0.3px",
+};
 
 export default function ProductsPanel() {
+    const [products, setProducts] = useState([]);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [loadingProducts, setLoadingProducts] = useState(true);
+    const [loadingProduct, setLoadingProduct] = useState(false);
+    const [msg, setMessage] = useState("");
+
+    // UI mode switch ---------------------------------------
+    // Will switch depending on viewing, editing, removing, or adding
+    const [activeMode, setActiveMode] = useState("none"); 
+
+    async function loadProducts() {
+        setMessage("");
+        setLoadingProducts(true);
+        try {
+            // !!!!!!!!!!!!!!!!!!!!!!!!!
+            const res = await getProductsReq();
+            setProducts(res || [])
+            setMessage("Success!");
+        }
+        catch (error) {
+            setMessage(error?.message || "Error loading products.");
+        }
+        finally {
+            setLoadingProducts(false);
+        }
+    }
+
+    async function viewProduct(productId) {
+        setMessage("");
+        setLoadingProduct(true);
+        try {
+            setActiveMode("view")
+            const res = await getProductReq(productId)
+            setSelectedProduct(res)
+            setMessage("Success!");
+        }
+        catch (error) {
+            setMessage(error?.message || "Error viewing product.");
+        }
+        finally {
+            setLoadingProduct(false);
+        }
+    }
+
+    useEffect(() => {
+        loadProducts();
+    }, []);
+
+    if (loadingProducts) {
+        return (
+        <Text 
+            style={luxuryBodyStyle}>
+            Loading Products...
+        </Text>);
+    }
+    
+    // FIGURE OUT LATER
+    function getProductId(p) {
+        return p?.productid || p?.product_id || p?.id;
+    }
+
     return (
-        <View padding="medium">
-            <Card>
-                <Heading level={2}>Products Panel</Heading>
-                <Text>This is the product panel section of the admin dashboard.</Text>
+        <Flex 
+            direction="row" 
+            gap="1rem" 
+            height="100%">
+
+            {/* Left card holding emails ---------------------------------------------*/}
+            <Card
+                width="45%" 
+                height="100%" 
+                padding="1rem" 
+                backgroundColor="whitesmoke"
+                >
+                <Flex 
+                    justifyContent="space-between" 
+                    alignItems="center"
+                    wrap="wrap">
+                       
+                    {/* {msg && ( 
+                        <Text 
+                            color="Black" 
+                            style={luxuryBodyStyle} 
+                            marginTop="0.5rem">
+                            {msg}
+                        </Text>
+                    )} */}
+                    <Text 
+                        style={luxuryHeadingStyle}>
+                        Products
+                    </Text>
+                    <Flex 
+                        direction="row"
+                        gap="0.75rem" 
+                        wrap="wrap" 
+                        justifyContent="flex-end">
+                        <Button 
+                            style={luxuryBodyStyle}
+                            onClick={() => {
+                                setSelectedProduct(null);
+                                setActiveMode("add");
+                            }}
+                            >
+                            Add
+                        </Button>
+                        <Button 
+                            style={luxuryBodyStyle}
+                            disabled={!selectedProduct}
+                            onClick={() => {
+                                if (!selectedProduct) return;
+                                setActiveMode("remove");
+                            }}
+                            >
+                            Remove
+                        </Button>
+                        <Button 
+                            style={luxuryBodyStyle}
+                            disabled={!selectedProduct}
+                            onClick={() => {
+                                if (!selectedProduct) return;
+                                setActiveMode("edit");
+                            }}
+                            >
+                            Edit
+                        </Button>
+                    </Flex>
+                </Flex>
+                <View overflow="auto" height="20rem" marginTop="1rem"> 
+                    {products.map((prod) => (
+                        <Button
+                            // FIGURE OUT LATER !!!!
+                            key={getProductId(prod)}
+                            variation="link"
+                            justifyContent="flex-start"
+                            width="100%"
+                            onClick={() => {
+                                setSelectedProduct(prod);
+                                setActiveMode("view");
+                            }}
+                            >
+                            {prod.name} — qty: {prod.quantity}
+                        </Button>
+                    ))}
+                </View>
             </Card>
-        </View>
+        
+            {/* Right card views, handles delete, or adds information ----------------------------------------- */}
+            <Card
+                width="45%" 
+                height="100%" 
+                padding="1rem" 
+                backgroundColor="whitesmoke"
+            >
+                <Flex 
+                    justifyContent="space-between" 
+                    alignItems="center">
+                    {activeMode === "add" &&
+                    (
+                        <Text 
+                            style={luxuryBodyStyle}>
+                            add
+                        </Text>
+                    )}    
+
+                    {activeMode === "remove" && //selectedProduct && 
+                    (
+                        <Text 
+                            style={luxuryBodyStyle}>
+                            remove
+                        </Text>
+                    )}    
+
+                    {activeMode === "edit" && //selectedProduct && 
+                    (
+                        <Text 
+                            style={luxuryBodyStyle}>
+                            edit
+                        </Text>
+                    )}
+
+                    {activeMode === "view" && //selectedProduct && 
+                    (
+                        <Text 
+                            style={luxuryBodyStyle}>
+                            edit
+                        </Text>
+                    )}    
+                </Flex>
+            </Card>
+        </Flex>
     );
 }
+
+//What are the exact function names you want me to call for: list, get by id, create, update, delete?
+
+//What product fields does the backend return? (ids + image fields + stock + featured + active)
+
+//For images: are we storing imageUrl directly, or are we doing upload to S3/Amplify and storing key/url?
+// We are gonna be storing imageURL, for now I upload images, if not will need 2 roundtrips uplload to s3 and store keys
+// treat as array of file objects 
+
+// Is remove product hard delete or soft delete (isActive=false)?
+// Called ishidden, get active products will hide is hidden.
+// isfeatured isnt used anywhere so far
+
+
+// Confirm data shape
+/*ex : 
+{
+  product_id: "string",
+  name: "string",
+  description: "string",
+  price: number,
+  imageUrl: "string",
+  stock: number,
+  isFeatured: boolean,
+  isActive: boolean,  // soft delete / hide
+  created_at: "string"
+}
+*/
+
+
+
+// server.js is basically api.js, what end points to hit directly 
+// handlers come from the controllers 
+
+// controllers.js has endpoints on top of each function, functions that handle endpoints
+// all wrapped in handle error, prevents server crashes
+// The functions comm with db
+
+// db.js
+// connects to database
+// creates tables
+// users table
+// products table
+// type in products will be men or womens fragrances
+// name is prodname
+// variation, 5ML spray or something else
+// price 
+// images, will be url's
+// Notes top heart and base
+// review table has a bunch of good information as well, responses only retaj can respond
+
+// request.js, 
+// do the function and fill in the variables
+
+
+// Ayman we import the function from AWS
+// Murad we have the functions in the files on the backend folder
+
+// local development will stick with murads functions 
---
+++ frontend/src/requests.js
@@ -0,0 +1,328 @@
+const backendURL = "http://localhost:5001"
+
+function handleError(fn){
+  return async (...args) => {
+    try{
+      return await fn(...args)
+    }catch(err){
+      console.error(err)
+      return null
+    }
+  }
+}
+
+
+// users
+async function validateLoginReq(id, email, password){
+    const response = await fetch(backendURL + `/users/login/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data?.user
+}
+
+async function changePasswordReq(id, password) {
+    const response = await fetch(backendURL + `/users/password/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({password})
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data
+}
+
+async function getUserReq(id) {
+    const response = await fetch(backendURL + `/users/${id}`);
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.user;
+}
+
+async function getUsersReq() {
+    const response = await fetch(backendURL + "/users");
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.users;
+}
+
+async function createUserReq({email, password, firstname, lastname, role, preferrednotes}){
+    const response = await fetch(backendURL + `/users`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password, firstname, lastname, role, preferrednotes})
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.user;
+}
+
+async function deleteUserReq(id){
+    const response = await fetch(backendURL + `/users/${id}`, {
+        method: "DELETE",
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data;
+}
+
+// products
+async function getProductReq(id){
+    const response = await fetch(backendURL + `/products/${id}`);
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.product;
+}
+
+async function updateProductReq(id, updatedFields){
+    const copy = {...updatedFields}
+    const fd = new FormData()
+    if (Object.hasOwn(copy, "images")){
+        for (let img of copy["images"]){
+            fd.append("images", img);
+        }
+        copy.images = []
+    }
+    fd.append("normalFields", JSON.stringify(copy));
+
+    const response = await fetch(backendURL + `/products/${id}`, {
+        method: "PUT",
+        body: fd
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data;
+}
+
+async function deleteProductReq(id){
+    const response = await fetch(backendURL + `/products/${id}`, {
+        method: "DELETE",
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data;
+}
+
+async function getActiveProductsReq(){
+    const response = await fetch(backendURL + `/products/active`);
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.products;
+}
+
+//images are a list of File objects
+async function createProductReq({type, name, variation, price, images, quantity, notes, isfeatured, ishidden}){
+    const normalFields = JSON.stringify({type, name, variation, price, quantity, notes, isfeatured, ishidden});
+    const fd = new FormData()
+    fd.append("normalFields", normalFields)
+    for (let img of images){
+        fd.append("images", img)
+    }
+    const response = await fetch(backendURL + `/products`, {
+        method: "POST",
+        body: fd
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.product;
+}
+
+async function getProductsReq() {
+    const response = await fetch(backendURL + `/products`);
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.products;
+}
+
+
+// reviews
+async function getProductReviewsReq(productid){
+    const response = await fetch(backendURL + `/reviews/product/${productid}`);
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.reviews;
+}
+
+async function getUserReviewsReq(customerid){
+    const response = await fetch(backendURL + `/reviews/user/${customerid}`);
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.reviews;
+}
+
+//as of now, can only update "responses"
+async function updateReviewReq(id, updatedFields){
+    const response = await fetch(backendURL + `/reviews/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(updatedFields)
+    });
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data
+}
+
+
+async function createReviewReq({customerid, productid, message, rating, images}){
+    const normalFields = JSON.stringify({customerid, productid, message, rating});
+    const fd = new FormData()
+    fd.append("normalFields", normalFields)
+    for (let img of images){
+        fd.append("images", img)
+    }
+    const response = await fetch(backendURL + `/reviews`, {
+        method: "POST",
+        body: fd
+    });
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.review
+}
+
+async function getReviewsReq(){
+    const response = await fetch(backendURL + `/reviews`);
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.reviews;
+}
+
+async function deleteReviewReq(id){
+    const response = await fetch(backendURL + `/reviews/${id}`, {
+        method: "DELETE",
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data;
+}
+
+// orders
+async function cancelOrderReq(id, cancelreason) {
+    const response = await fetch(backendURL + `/orders/cancel/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({cancelreason})
+    });
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data;
+}
+
+async function completeOrderReq(id) {
+    const response = await fetch(backendURL + `/orders/complete/${id}`, {
+        method: "PUT",
+    });
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data;
+}
+
+async function getOrderReq(id){
+    const response = await fetch(backendURL + `/orders/${id}`);
+    const data = await response.json()
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.order;
+}
+
+async function createOrderReq({customerid, items, total}){
+    const response = await fetch(backendURL + `/orders`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({customerid, items, total})
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data.data.order;
+}
+
+async function deleteOrderReq(id){
+    const response = await fetch(backendURL + `/orders/${id}`, {
+        method: "DELETE",
+    });
+    const data = await response.json();
+    if (!data.success){
+        throw new Error(data.message || "req failed");
+    }
+    return data;
+}
+
+validateLoginReq = handleError(validateLoginReq);
+changePasswordReq = handleError(changePasswordReq);
+getUserReq = handleError(getUserReq);
+getUsersReq = handleError(getUsersReq);
+createUserReq = handleError(createUserReq);
+deleteUserReq = handleError(deleteUserReq);
+
+getProductReq = handleError(getProductReq);
+updateProductReq = handleError(updateProductReq);
+deleteProductReq = handleError(deleteProductReq);
+getActiveProductsReq = handleError(getActiveProductsReq);
+createProductReq = handleError(createProductReq);
+getProductsReq = handleError(getProductsReq);
+
+getProductReviewsReq = handleError(getProductReviewsReq);
+getUserReviewsReq = handleError(getUserReviewsReq);
+updateReviewReq = handleError(updateReviewReq);
+createReviewReq = handleError(createReviewReq);
+getReviewsReq = handleError(getReviewsReq);
+deleteReviewReq = handleError(deleteReviewReq);
+
+cancelOrderReq = handleError(cancelOrderReq);
+completeOrderReq = handleError(completeOrderReq);
+getOrderReq = handleError(getOrderReq);
+createOrderReq = handleError(createOrderReq);
+deleteOrderReq = handleError(deleteOrderReq);
+
+
+export {
+    validateLoginReq, changePasswordReq, getUserReq, getUsersReq, createUserReq, deleteUserReq,
+    getProductReq, updateProductReq, deleteProductReq, getActiveProductsReq, createProductReq, getProductsReq,
+    getProductReviewsReq, getUserReviewsReq, updateReviewReq, createReviewReq, getReviewsReq, deleteReviewReq,
+    cancelOrderReq, completeOrderReq, getOrderReq, createOrderReq, deleteOrderReq
+}
